43896a9bed3d505600f74662a6e452bd50c284df
VictorXie03 <victorxie75@gmail.com>
2024-03-22 18:51:00 -0400

    updated cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ mongoose.set('strictQuery', false);
 mongoose.connect(process.env.DB_CONNECTION, () => console.log('connected to DB!'))
 
 
-app.use(cors({ credentials: true, origin: true }));
+app.use(cors({ credentials: true, origin: process.env.REACT_APP_CORS_LINK }));
 app.use('/', express.static(path.join(__dirname, 'static')))
 app.use(bodyParser.json())
 app.use(
