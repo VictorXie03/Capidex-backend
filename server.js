fc3040b403ff6f99a37dd418bc8e69cb688a4e09
VictorXie03 <victorxie75@gmail.com>
2024-04-01 21:21:11 -0400

    updated mongodb

--- server.js
+++ server.js
@@ -15,11 +15,7 @@ mongoose.connection.on('disconnected', () => console.log('disconnected'));
 mongoose.connection.on('reconnected', () => console.log('reconnected'));
 mongoose.connection.on('disconnecting', () => console.log('disconnecting'));
 mongoose.connection.on('close', () => console.log('close'));
-try {
-    await mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => { console.log('connected to DB!') });
-} catch (error) {
-    handleError(error);
-}
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => { console.log('connected to DB!') });
 app.use(cors({ credentials: true, origin: process.env.REACT_APP_CORS_LINK }));
 app.use('/', express.static(path.join(__dirname, 'static')))
 app.use(bodyParser.json())
